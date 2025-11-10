e2e8eddac6ebc3d963e033f69a2ff4a41d5318a3
Felix Cruz <fcruzm95@gmail.com>
2025-11-10 12:06:17 +0000

    removed code

--- application/useTodo.ts
+++
@@ -1,4 +0,0 @@
-export const useTodo = () => {
-  
-  return {};
-}
--- domain/todo.ts
+++
@@ -1,8 +0,0 @@
-export interface TodoElement {
-  id: string;
-  title: string;
-  description: string;
-  completed: boolean;
-}
-
-export type TodoList = TodoElement[]
